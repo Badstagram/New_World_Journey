ed3693bfe73265cfd10165b087fa6fa719072bb5
Kyle McCullough <kylematthewmccullough@gmail.com>
2020-02-08 16:07:50 -0600

    More ship

--- src/main/java/org/ttpi/new_world_journey/engine/ships/Mayflower.java
+++ src/main/java/org/ttpi/new_world_journey/engine/ships/Mayflower.java
@@ -1,7 +1,6 @@
 package org.ttpi.new_world_journey.engine.ships;
 
 public class Mayflower extends Ship {
-    private int MAX_HEALTH;
 
     public Mayflower() {
         super(1,1,1,1);
--- src/main/java/org/ttpi/new_world_journey/engine/ships/Ship.java
+++ src/main/java/org/ttpi/new_world_journey/engine/ships/Ship.java
@@ -21,18 +21,60 @@ public class Ship {
 
     }
 
-    public int progressShip(int n){
+    public int progressShip(int n) {
         this.currentDistance += n;
         return this.currentDistance;
     }
 
-    public int consumeFood(int n){
-        if(this.startFood - n <= 0){
+    public int consumeFood(int n) {
+        if (this.startFood - n <= 0) {
             this.startFood = 0;
             return 0;
-        }else{
+        } else {
             this.startFood -= n;
             return this.startFood;
+        }
+    }
+
+
+    public void changeHealth (int n){
+        if (healthOfShip + n > MAX_HEALTH) {
+            healthOfShip = MAX_HEALTH;
+        } else if (healthOfShip + n < 0) {
+                healthOfShip = 0;
+        } else {
+            healthOfShip += n;
+        }
+    }
+    public void changePassengers(int n){
+        if(passengers + n > MAX_PASSENGERS){
+            passengers = MAX_PASSENGERS;
+        }else if(passengers + n < 0){
+            passengers = 0;
+        }else{
+            passengers += n;
+        }
+    }
+
+    public void changeHappiness(int n){
+        if(happiness + n> 100){
+            happiness = 100;
+        }else if(happiness + n< 0){
+            happiness = 0;
+        }else{
+            happiness += n;
+        }
+    }
+
+    public int getCurrentDistance() {
+        return currentDistance;
+    }
+
+    public void changeCoins(int n){
+        if(coins + n < 0){
+            coins = 0;
+        }else{
+            coins += n;
         }
     }
 
